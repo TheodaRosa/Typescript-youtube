ec8b15fedcbcf86e00c4248c50610bbaa7f37e76
TheodaRosa <theo.smidt@fatec.sp.gov.br>
2024-06-12 14:13:45 -0300

    New classes

---
+++ Build/aula16.js
@@ -0,0 +1,16 @@
+"use strict";
+class computer {
+    nome = "Roberto-1";
+    ram = 0;
+    cpu = 0;
+    ligado = false;
+}
+const a1 = new computer();
+const a2 = new computer();
+const a3 = new computer();
+a1.nome = "Gui";
+a2.nome = "lher";
+a3.nome = "gamyer";
+console.log(a1.nome);
+console.log(a2.nome);
+console.log(a3.nome);
---
+++ src/aula16.ts
@@ -0,0 +1,18 @@
+class computer{
+    nome:string="Roberto-1"
+    ram:number=0
+    cpu:number=0
+    ligado:boolean=false
+}
+
+const a1=new computer()
+const a2=new computer()
+const a3=new computer()
+
+a1.nome="Gui"
+a2.nome="lher"
+a3.nome="gamyer"
+
+console.log(a1.nome)
+console.log(a2.nome)
+console.log(a3.nome)
